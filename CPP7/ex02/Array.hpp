4ea032ed7beb9afacd5cc9df234044d326326ea9
lgottsch99 <lgottsch@student.42prague.com>
2025-07-26 17:20:43 +0200

    jul 26

--- CPP7/ex02/Array.hpp
+++ CPP7/ex02/Array.hpp
@@ -1,5 +1,30 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <cstdlib>
+
+template <typename T>
+class Array
+{
+	private:
+		T* _elements;
+
+	public:
+		Array(void); //void constructor
+		Array(unsigned int n); // Creates an array of n elements initialized by default.
+		Array(const Array& other); //copy construct
+		Array &operator=( const Array& other); //copy assignment
+		~Array(); //destructor
+
+
+		const size_t size(void); //returns length of current array
+
+		class IndexOutOfBounds : public std::exception 
+		{
+			public:
+				const char * what(void) const throw();
+		};
+
+}
 
 #endif
