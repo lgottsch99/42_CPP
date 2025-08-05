b2c6350219403e34083a6c7316a59e311ef7cd65
lgottsch99 <lgottsch@student.42prague.com>
2025-08-05 14:31:43 +0200

    aug 5

--- CPP8/ex02/MutantStack.hpp
+++ CPP8/ex02/MutantStack.hpp
@@ -9,9 +9,10 @@
 
 // https://en.cppreference.com/w/cpp/container/stack.html 
 
-template <typename T, typename Container=std::deque<T> >
-class MutantStack: public std::stack<T, Container> //inheriting from stack
+template <typename T, typename Container=std::deque<T> > //container param like std::stack defualt is deque but any is fine
+class MutantStack: public std::stack<T, Container> //inheriting from stack, 
 {
+	//container c inherited from std::stack
 
 	public:
 		MutantStack(void);//default
@@ -19,35 +20,26 @@ class MutantStack: public std::stack<T, Container> //inheriting from stack
 		MutantStack &operator=( const MutantStack& other); //copy assignment
 		~MutantStack(); //destructor
 
-		//iterators
+		//iterator alias: to make code more reusable/ versatile w different container types
 		typedef typename Container::iterator iterator;
-		typedef typename Container::const_iterator const_iterator;
-		typedef typename Container::reverse_iterator rev_iterator;
-		typedef typename Container::const_reverse_iterator const_rev_iterator;
-		
+
 		//iterator fts
 		iterator begin();
 		iterator end();
-		const_iterator cbegin();
-		const_iterator cend();
-		rev_iterator rbegin();
-		rev_iterator rend();
-		const_rev_iterator crbegin();
-		const_rev_iterator crend();
+
+		//iterator overloads? ++ -- ... ->not needed, inherited from stack
 
 
-		//members
+		//members -> inherited? 
 		//push pop etc (get from deque)
 
 		//push
-		void	push(T elem); //deque push_back
 
 		//pop//deque pop_back
 
 		//empty //deque e,pty
 		//size //deque size
 		//top //?
-
 
 
 };
@@ -63,10 +55,11 @@ MutantStack<T, Container>::MutantStack(void)
 
 //copy construct
 template <typename T, typename Container>
-MutantStack<T, Container>::MutantStack(const MutantStack<T, Container>& other)
+MutantStack<T, Container>::MutantStack(const MutantStack<T, Container>& other):
+std::stack<T, Container>(other) //better: letting base class handle the copying
 {
-	(void)other;
 	std::cout << "(MutantStack) Copy constructor\n";
+	// this->c = other.c;
 }
 
 //copy assignment
@@ -77,6 +70,7 @@ MutantStack<T, Container> &MutantStack<T, Container>::operator=( const MutantSta
 	std::cout << "(MutantStack) Copy assignment\n";
 	if (this != &other)
 	{
+		this->c = other.c;
 	}
 	return (*this);
 }
@@ -103,43 +97,6 @@ typename MutantStack<T, Container>::iterator MutantStack<T, Container>::end()
 	return (this->c.end()); //c is container in std::stack
 }
 
-
-template <typename T, typename Container>
-typename MutantStack<T, Container>::const_iterator MutantStack<T, Container>::cbegin()
-{
-	return (this->c.cbegin());
-}
-
-template <typename T, typename Container>
-typename MutantStack<T, Container>::const_iterator MutantStack<T, Container>::cend()
-{
-	return (this->c.cend());
-}
-
-template <typename T, typename Container>
-typename MutantStack<T, Container>::rev_iterator MutantStack<T, Container>::rbegin()
-{
-	return (this->c.rbegin());
-}
-
-template <typename T, typename Container>
-typename MutantStack<T, Container>::rev_iterator MutantStack<T, Container>::rend()
-{
-	return (this->c.rend());
-}
-
-
-template <typename T, typename Container>
-typename MutantStack<T, Container>::const_rev_iterator MutantStack<T, Container>::crbegin()
-{
-	return (this->c.crbegin());
-}
-
-template <typename T, typename Container>
-typename MutantStack<T, Container>::const_rev_iterator MutantStack<T, Container>::crend()
-{
-	return (this->c.crend());
-}
 
 
 #endif
--- CPP8/ex02/main.cpp
+++ CPP8/ex02/main.cpp
@@ -1,5 +1,6 @@
 
 #include "MutantStack.hpp"
+#include <list>
 
 /*
 
@@ -14,14 +15,98 @@ other specifically designed container class.
 The container shall support the following operations:
 empty
 size
-back
-push_back
-pop_back
+top
+push
+pop
 
 
 */
 
 int main(void)
 {
+	//subject main ----
 	MutantStack<int> mstack;
+	std::cout << "\nMUTANT STACK TEST ----------\n\n\n";
+
+	if (mstack.empty())
+		std::cout << "mstack is empty\n";
+
+	std::cout << "pushing to mstack..\n";
+	mstack.push(5);
+	mstack.push(17);
+
+	if (!mstack.empty())
+		std::cout << "mstack not empty\n";
+
+	std::cout << "top: " << mstack.top() << std::endl;
+
+	mstack.pop();
+
+	std::cout << "size: " << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	
+	//[...] 
+	mstack.push(0);
+
+	std::cout << "\nprinting stack after adding: \n";
+	MutantStack<int>::iterator it = mstack.begin(); 
+	MutantStack<int>::iterator ite = mstack.end();
+	++it; //just testing if ++/-- is working and doesnt throw errors
+	--it;
+	while (it != ite) 
+	{
+		std::cout << *it << std::endl;
+		++it; 
+	}
+	std::cout << "size: " << mstack.size() << std::endl;
+
+	std::stack<int> s(mstack); //?? -> since mstack inherits from stack it can be copy constructed with stack, no issue
+	std::cout << "size std::stack<int> s(mstack): " << s.size() << std::endl;
+
+
+
+	// list test ----------------
+	std::list<int> list;
+	std::cout << "\n\nLIST TEST ----------\n\n\n";
+
+	if (list.empty())
+	std::cout << "list is empty\n";
+
+	std::cout << "pushing to list..\n";
+	list.push_back(5);
+	list.push_back(17);
+
+	if (!list.empty())
+		std::cout << "list not empty\n";
+
+	std::cout << "top: " << list.back() << std::endl;
+
+	list.pop_back();
+
+	std::cout << "size: " << list.size() << std::endl;
+
+	list.push_back(3);
+	list.push_back(5);
+	list.push_back(737);
+
+	//[...] 
+	list.push_back(0);
+
+	std::cout << "\nprinting stack after adding: \n";
+	std::list<int>::iterator lit = list.begin(); 
+	std::list<int>::iterator lite = list.end();
+	++lit;
+	--lit;
+	while (lit != lite) 
+	{
+		std::cout << *lit << std::endl;
+		++lit; 
+	}
+	std::cout << "size: " << list.size() << std::endl;
+
+	return 0;
+	
 }
